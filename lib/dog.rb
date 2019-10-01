fa7b13e3cda2bb93e532c08105bc9014122014c1
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-10-01 21:13:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def self.all
+    @@all 
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+
+  
+end
